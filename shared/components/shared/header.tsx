fea479a47ec23fb6d92da6cf0e2696945d30c2c9
Oleg <nikita.nikon.rus@gmail.com>
2025-09-17 11:59:45 +0300

    added filtration

--- shared/components/shared/header.tsx
+++ shared/components/shared/header.tsx
@@ -4,6 +4,8 @@ import {cn} from '@/shared/lib/utils'
 import Image from 'next/image'
 import { FC } from 'react'
 import { ArrowRight, ShoppingCart, User } from 'lucide-react'
+import Link from 'next/link'
+import { SearchInput } from './search-input'
 
 interface Props {
   className?: string
@@ -13,12 +15,18 @@ export const Header: FC<Props> = ({className}) => {
   return (
     <header className={cn('border border-b', className)}>
       <Container className='flex items-center justify-between py-8'>
-        <div className='flex items-center gap-4'>
-          <Image src='/logo.png' alt='Logo' width={35} height={35}/>
-          <div>
-            <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
-            <p className="text-sm text-gray-400 leading-3">вкусней уже некуда</p>
+        <Link href='/'>
+          <div className='flex items-center gap-4'>
+            <Image src='/logo.png' alt='Logo' width={35} height={35}/>
+            <div>
+              <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
+              <p className="text-sm text-gray-400 leading-3">вкусней уже некуда</p>
+            </div>
           </div>
+        </Link>
+
+        <div className='mx-10 flex-1'>
+          <SearchInput />
         </div>
 
         <div className="flex items-center gap-3">
