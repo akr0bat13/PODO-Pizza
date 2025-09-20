7cfd5c9830556aeed82b15166e2a6e5b0994ea60
Oleg <nikita.nikon.rus@gmail.com>
2025-09-20 18:07:05 +0300

    created pizza`s modal

---
+++ app/(root)/product/[id]/page.tsx
@@ -0,0 +1,24 @@
+import { prisma } from "@/prisma/prisma-client"
+import { Container, GroupVariants, ProductImage, Title } from "@/shared/components/shared"
+import { notFound } from "next/navigation"
+
+export default async function ProductPage({params: {id}}: {params: {id: string}}) {
+  const product = await prisma.product.findFirst({where: {id: Number(id)}})
+
+  if(!product) return notFound()
+
+  return <Container className="flex flex-col my-10">
+    <div className="flex flex-1">
+      <ProductImage imageUrl={product.imageUrl} size={30}/>
+      <div className="w-[490px] bg-[#fcfcfc] p-7">
+        <Title text={product.name} size="md" className="font-extrabold mb-1" />
+        <GroupVariants items={[
+          {name: 'Маленькая', value: '1'},
+          {name: 'Средняя', value: '2'},
+          {name: 'Большая', value: '3'},
+        ]}/>
+      </div>
+
+    </div>
+  </Container>
+}
